094920bebbacff65e6e42553afc4edf55f6df5d5
Guitrembala-60hz <93789293+Guitrembala-60hz@users.noreply.github.com>
2021-11-30 17:04:04 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -3,38 +3,25 @@ const World = Matter.World;
 const Bodies = Matter.Bodies;
 
 var mecanismo, mundo;
-var solo, bola;
+var box1, box2, solo;
 
 function setup() {
   var canvas = createCanvas(400, 400);
 
   mecanismo = Engine.create();
   mundo = mecanismo.world;
+ 
+  box1 = new Box(200,300,50,50);
+  box2 = new Box(240,100,50,100);
 
-  var solo_opcao ={
-    isStatic: true
-  }
-
-  solo = Bodies.rectangle(200,390,200,20, solo_opcao);
-
-  World.add(mundo,solo);
+  solo = new Ground(200,410,400,20);
   
-  var bola_opcao ={
-    restitution:0.8
   }
-  bola = Bodies.circle(200,100,20, bola_opcao);
-
-  World.add(mundo,bola);
-}
 function draw(){
 background(0);
 
 Engine.update(mecanismo);
 
-rectMode(CENTER);
-rect(solo.position.x,solo.position.y,400,20);
-ellipseMode(RADIUS);
-ellipse(bola.position.x, bola.position.y,20,20);
-
-
+box1.display();
+box2.display();
 }
